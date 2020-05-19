e8228a751362bd3fd48dabd376189ec4f5a0a70a
AzAz <adam_an02@163.com>
2020-05-20 04:05:14 +0800

    add trusted .crt (#5)

--- run-openssl.sh
+++ run-openssl.sh
@@ -4,4 +4,5 @@ openssl req -new -newkey rsa:2048 -sha256 -nodes -out one-cert.csr -keyout one-c
 openssl x509 -req -days 3650 -in one-cert.csr -signkey one-cert.key \
  -out one-cert.crt -extfile openssl.cnf -extensions v3_req
 
+openssl x509 -trustout  -req -days 3650 -in one-cert.csr -signkey one-cert.key -out one-cert-trusted.crt
 openssl x509 -in one-cert.crt -text -noout
